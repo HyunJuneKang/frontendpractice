6681681903b6cab197d890f828756392dd43279b
HyeonJuneKang <halfkang67@naver.com>
2026-07-05 00:33:23 +0900

    first commit

---
+++ src/main/java/com/shinhan/frontendpractice/FrontendpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan.frontendpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendpracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendpracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shinhan/frontendpractice/FrontendpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhan.frontendpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendpracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
