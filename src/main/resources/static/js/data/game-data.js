c87d92c9234a99e4fa9f91a03cbff42dc109ed8c
HyunJuneKang <halfkang67@naver.com>
2026-07-06 09:02:59 +0900

    벌목 렌더링 및 상태바 추가 및 더미데이터 추가

---
+++ src/main/resources/static/js/data/game-data.js
@@ -0,0 +1,38 @@
+const woodcuttingList = [
+    {
+        id: "1",
+        name:"참나무",
+        icon:"🌳",
+        requiredLevel:"1",
+        gainExp:10,
+        gainItemId:51,
+        gainItemAmount:1
+    },
+    {
+        id: "2",
+        name:"야자수",
+        icon:"🌴",
+        requiredLevel:"10",
+        gainExp:20,
+        gainItemId:52,
+        gainItemAmount:1
+    },
+    {
+        id: "3",
+        name:"트리",
+        icon:"🎄",
+        requiredLevel:"20",
+        gainExp:30,
+        gainItemId:53,
+        gainItemAmount:1
+    },
+    {
+        id: "4",
+        name: "침엽수",
+        icon: "🌲",
+        requiredLevel: "30",
+        gainExp: 40,
+        gainItemId: 54,
+        gainItemAmount: 1
+    }
+];
--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -1,10 +1,38 @@
 $(()=>{
-    bindEvent();
+    init();
 });
+//init
+function init(){
+    renderGameScreen();
+    bindEvent();
+}
+//화면 렌더링
+function renderGameScreen() {
+    renderSkillScreen();
+}
+//스킬 화면 렌더링
+function renderSkillScreen(){
+    renderWoodcuttingScreen();
+}
+//스킬 세부 화면 렌더링
+function renderWoodcuttingScreen() {
+    woodcuttingList.forEach((p)=>{
+        $(".active-card-container").append(`
+            <button type="button" id="tree-1" class="card" data-id="${p.id}">
+            <div>${p.name} ${p.icon}</div>
+            <div>${p.gainExp}exp</div>
+            <div>휙득 아이템 id:${p.gainItemId}</div>
+            <div>수량 : ${p.gainItemAmount}</div>
+            <div class="progress-bar progress-bar"> </div>
+            </button>
+        `);
+    });
+}
 // 버튼 이벤트
 function bindEvent(){
     $(".sidebar-section-title-button-hide").on("click",handleToggleSectionButtons);
     $(".sidebar-button").on("click",handleRenderSectionScreen);
+    $(".active-card-container .card").on("click",handleChangeProcess);
 }
 
 //handler
@@ -16,6 +44,9 @@ function handleRenderSectionScreen(e){
     const sectionId = $(e.currentTarget).data("target");
     switchSectionScreen(sectionId);
 }
+function handleChangeProcess(e){
+    switchChangeProcess($(e.currentTarget).data("id"));
+}
 //Events
 //화면 변경
 function switchSectionScreen(sectionId){
@@ -24,6 +55,9 @@ function switchSectionScreen(sectionId){
     $targetSection.removeClass("hidden"); //보여줄 화면에 hidden 제거
     //헤더정보 변경
     console.log(sectionId,"변경 완료");
+}
+function switchChangeProcess(){
+    //
 }
 //사이드바 숨기기
 function toggleButtonContents(containerId){
